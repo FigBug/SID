a62dc0fd8882f9751d4a1bc911d794a5f8e28ef3
Roland Rabien <roland@rabien.com>
2019-11-19 09:39:09 -0800

    Add Windows build

---
+++ Scripts/pcbuildprojects.bat
@@ -0,0 +1,22 @@
+echo on
+set MSBUILD_EXE=C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\MSBuild\15.0\Bin\MSBuild.exe
+
+set ROOT=%cd%
+cd "%ROOT%"
+
+cd Scripts
+mkdir bin
+mkdir bin\win
+mkdir bin\zip
+
+cd "..\plugin\Builds\VisualStudio2017"
+"%MSBUILD_EXE%" SID.sln /p:VisualStudioVersion=15.0 /m /t:Build /p:Configuration=Release64 /p:Platform=x64 /p:PreferredToolArchitecture=x64
+if %errorlevel% neq 0 exit /b %errorlevel%
+"%MSBUILD_EXE%" SID.sln /p:VisualStudioVersion=15.0 /m /t:Build /p:Configuration=Release /p:PlatformTarget=x86 /p:PreferredToolArchitecture=x64
+if %errorlevel% neq 0 exit /b %errorlevel%
+
+cd "%ROOT%\Scripts\bin"
+
+copy "%ROOT%\plugin\Builds\VisualStudio2017\x64\Release64\VST\SID_64b.dll" zip
+copy "%ROOT%\plugin\Builds\VisualStudio2017\Win32\Release\VST\SID_32b.dll" zip
+
